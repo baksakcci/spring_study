fe1bca94414b08efc0c34e9e85bda12d399657dd
baksakcci <pshsh910@gmail.com>
2022-07-30 18:59:29 +0900

    refactor: change return type from ResponseEntity<> to Response

--- src/main/java/com/example/mentoring/controller/BoardController.java
+++ src/main/java/com/example/mentoring/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.mentoring.controller;
 
 import com.example.mentoring.entity.Board;
+import com.example.mentoring.response.Response;
 import com.example.mentoring.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,33 +15,38 @@ public class BoardController {
     private final BoardService boardService;
 
     // 게시물 전체 보여주기
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/boards")
-    public ResponseEntity<?> getBoards() {
-        return new ResponseEntity<>(boardService.getBoards(), HttpStatus.OK);
+    public Response getBoards() {
+        return Response.success(boardService.getBoards());
     }
 
     // 게시물 1개 보여주기
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/boards/{id}")
-    public ResponseEntity<?> getBoard(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(boardService.getBoard(id), HttpStatus.OK);
+    public Response getBoard(@PathVariable("id") Long id) {
+        return Response.success(boardService.getBoard(id));
     }
 
-    // 게시물 저장 버튼 누르기
+    // 게시물 저장 버튼 누르기 - POST
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/boards")
-    public ResponseEntity<?> saveBoard(@RequestBody Board board) {
-        return new ResponseEntity<>(boardService.saveBoard(board), HttpStatus.CREATED);
+    public Response saveBoard(@RequestBody Board board) {
+        return Response.success(boardService.saveBoard(board));
     }
 
     // 게시물 수정하고 완료 버튼 누르기
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/boards/{id}")
-    public ResponseEntity<?> updateBoard(@PathVariable("id") Long id, @RequestBody Board updateBoard) {
-        return new ResponseEntity<>(boardService.updateBoard(updateBoard, id), HttpStatus.OK);
+    public Response updateBoard(@PathVariable("id") Long id, @RequestBody Board updateBoard) {
+        return Response.success(boardService.updateBoard(updateBoard, id));
     }
 
     // 게시물 삭제 버튼 누르기
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/boards/{id}")
-    public ResponseEntity<?> deleteBoard(@PathVariable("id") Long id) {
+    public Response deleteBoard(@PathVariable("id") Long id) {
         boardService.deleteBoard(id);
-        return new ResponseEntity<>("삭제 완료", HttpStatus.OK);
+        return Response.success("삭제 완료");
     }
 }
